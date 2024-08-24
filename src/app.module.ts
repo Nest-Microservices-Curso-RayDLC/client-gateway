df58a7da79d6445b3670489e7689b94f092063ce
RayDLC <theraydavid@gmail.com>
2024-08-24 17:40:14 -0500

    Add: Health Check

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './modules/products/products.module';
 import { OrdersModule } from './modules/orders/orders.module';
 import { NatsModule } from './transports/nats.module';
+import { HealthCheckModule } from './health-check/health-check.module';
 
 @Module({
   imports: [
     ProductsModule,
     OrdersModule,
-    NatsModule
+    NatsModule,
+    HealthCheckModule
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
@@ -9,7 +9,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app
-    .setGlobalPrefix('api')
+    .setGlobalPrefix('api', {
+      exclude: [{
+        path: '',
+        method: RequestMethod.GET
+      }]
+    })
     .useGlobalPipes(
       new ValidationPipe({
         whitelist: true,
